cbc29ca16775ff0927834d68f2f4cf4ac661239b
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-02-24 16:37:33 +0000

    Add a role to create-users.sh

--- scripts/create-users.sh
+++ scripts/create-users.sh
@@ -6,4 +6,5 @@ echo "Setting up SSCS Users and role assignments..."
 
 ##XUI only has IA whitelisted, perhaps AM. Causing issues with retrieving role assignments.
 ./actions/organisational-role-assignment.sh "${CASE_WORKER_USER}" "${CASE_WORKER_PASSWORD}" "PUBLIC" "senior-tribunal-caseworker" '{"jurisdiction":"SSCS","primaryLocation":"765324"}'
+./actions/organisational-role-assignment.sh "${CASE_WORKER_USER}" "${CASE_WORKER_PASSWORD}" "PUBLIC" "caseworker-sscs-registrar" '{"jurisdiction":"SSCS","primaryLocation":"765324"}'
 ./actions/organisational-role-assignment.sh "${CASE_WORKER_USER}" "${CASE_WORKER_PASSWORD}" "PUBLIC" "senior-tribunal-caseworker" '{"jurisdiction":"IA","primaryLocation":"765324"}'
